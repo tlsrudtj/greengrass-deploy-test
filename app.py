050893001c9d1f12104a5eb8f8d7ffcce18b6d5f
tlsrudtj <tlsrudtj@gmail.com>
2026-05-22 15:22:10 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import time
+from datetime import datetime
+
+VERSION = "1.0.0.1"
+
+while True:
+    now = datetime.now().strftime("%Y-%m-%d %H:%M")
+    print(f"그린그라스 배포 테스트 {now} VER{VERSION}")
+    time.sleep(3)
